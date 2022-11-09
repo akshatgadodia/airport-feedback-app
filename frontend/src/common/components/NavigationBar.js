9370cc14350712733fe4ac54263b1dc8f9594f16
arunabh-lejhro <arunabh.jain@lejhro.com>
2022-11-09 23:37:05 +0530

    navbar added

--- frontend/src/common/components/NavigationBar.js
+++ frontend/src/common/components/NavigationBar.js
@@ -1,16 +1,14 @@
 import React, { useContext } from "react";
 import "../stylesheets/NavigationBar.css";
 import { Context } from "../../App";
-import { useNavigate, useLocation } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const NavigationBar = () => {
   const { loggedInDetails, dispatch } = useContext(Context);
-  const location = useLocation();
   const navigate = useNavigate();
   console.log(loggedInDetails);
   const LogoutHandler = () => {
@@ -22,20 +20,42 @@ const NavigationBar = () => {
   };
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className={(loggedInDetails.userType) ? "navbar" : "navbar-hidden"}>
-    <Container>
-      <Navbar.Brand href="/home">Airport</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
-          <Nav.Link eventKey="home" href="/home">Home</Nav.Link>
-          <Nav.Link eventKey="feedback" href="/feedback"> {(loggedInDetails.userType === "user") ? "Give Feedback" : "View Feedbacks"} </Nav.Link>
-          <Nav.Link eventKey="about" href="/about"> About </Nav.Link>
-          <Nav.Link eventKey="logout" href="" onClick={LogoutHandler}> Logout </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+      className={loggedInDetails.userType ? "navbar" : "navbar-hidden"}
+    >
+      <Container>
+        <Navbar.Brand href="/home">
+          <div className="navbar-logo-div">
+            <img src="Images/airline-logo.png" height={50} alt="" />
+            <p>SKIT INTERNATIONAL AIRPORT</p>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link eventKey="home" href="/home">
+              <p>HOME</p>
+            </Nav.Link>
+            <Nav.Link eventKey="feedback" href="/feedback">
+              <p>FEEDBACK</p>
+            </Nav.Link>
+            <Nav.Link eventKey="about" href="/about">
+              <p>ABOUT</p>
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link eventKey="logout" href="" onClick={LogoutHandler}>
+              <p>
+                LOGOUT &nbsp; <LogoutIcon />
+              </p>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
